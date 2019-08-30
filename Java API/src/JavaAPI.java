bc98ca4aeb245db8cea44fcc0f66643bf0b71a93
hailey.forar <hailey.forar@MU-C1-20.mullen.pvt.k12.co.us>
2019-08-30 10:22:31 -0600

    edited program - started challenges

--- Java API/src/JavaAPI.java
+++ Java API/src/JavaAPI.java
@@ -4,7 +4,37 @@ public class JavaAPI
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				String string1 = "derive joy from code";
+				String string2 = "eat more cake";
+				
+				//Challenge 1
+				System.out.println(string1.substring(0,3));
+				
+				//Challenge 2
+				System.out.println(string2.substring(4,8));
+				
+				//Challenge 3
+				System.out.println(string2);
+				
+				//Challenge 4
+				System.out.println(string2.indexOf(0));
+				
+				//Challenge 5
+				System.out.println(string2.indexOf(12));
+				
+				//Challenge 6
+				
+				
+				//Challenge7
+				
+				
+				//Challenge 8
+				System.out.println(string1.toUpperCase());
+				
+				//Challenge 9
+				
+				
+				//Challenge 10
 
 			}
 
